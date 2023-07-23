d742c545ddc929c6ef9d69795ca13c1afaf119f3
Denilson Arguelho <denilsons.arguelho@gmail.com>
2023-07-23 09:12:39 -0400

    evento de inserção de letra

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,14 +1,28 @@
 import React from 'react'
+import {useState, useRef } from "react";
 import './Game.css'
+
+
 const Game = ({
   verifyLetter, pickedWord,
   pickedCategory, letters,
   guessedLetters, wrongLetters,
   guesses,
   score }) => {
+    //States
+  const [letter, setLetter] = useState("")
+  const letterInputRef = useRef(null)
 
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+
+    verifyLetter(letter)
+
+    setLetter("")
+
+    letterInputRef.current.focus()
+  }
   return (
-
     <div className="game">
       <p className="points">
         <span>Pontuação: {score}</span>
@@ -17,23 +31,27 @@ const Game = ({
       <h3 className="tip">Dica Sobre a palavra: <span>{pickedCategory}</span></h3>
       <p>Você ainda tem {guesses} tentativas</p>
       <div className="wordContainer">
-      {/* { console.log(letters.map((letter, i)=>(
-        console.log(letter)
-      )))} */}
+
         {letters.map((letter, i) => (
           guessedLetters.includes(letter) ? (
             <span key={i} className="letter">{letter}</span>
 
-          ): (
+          ) : (
             <span key={i} className="blankSquare"></span>
           )
-       ))}
+        ))}
       </div>
 
       <div className="letterContainer">
         <p>Tente advinhar uma letra da palavra:</p>
-        <form>
-          <input type="text" name='letter' maxLength="1" required />
+        <form onSubmit={handleSubmit}>
+          <input type="text" name='letter'
+            maxLength="1"
+            required
+            onChange={(e) => setLetter(e.target.value)}
+            value={letter}
+            ref={letterInputRef}
+            />
           <button>Jogar!</button>
         </form>
       </div>
